a301a07e855c6d418cdcf0a642033f505dafae4d
Baracudaaa <gatsrubakovitch@ya.ru>
2026-03-28 18:30:50 +0300

    дописать create_tables.py

---
+++ migrations/create_tables.py
@@ -0,0 +1,45 @@
+import asyncio
+import logging
+import os
+import sys
+
+from app.infrastructure.database.connection import get_pg_connection
+from config.config import Config, load_config
+from psycopg import AsyncConnection, Error
+
+config: Config = load_config()
+
+logging.basicConfig(
+    level=logging.getLevelName(level=config.log.level),
+    format=config.log.format,
+)
+
+logger = logging.getLogger(__name__)
+
+if sys.platform.startswith("win") or os.name == "nt":
+    asyncio.set_event_loop_policy(asyncio._WindowsSelectorEventLoopPolicy())
+
+async def main():
+    connection: AsyncConnection | None = None
+
+    try:
+        connection = await get_pg_connection(
+            db_name=config.db.name,
+            host=config.db.host,
+            port=config.db.port,
+            user=config.db.user,
+            password=config.db.password,
+        )
+        async with connection:
+            async with connection.transaction():
+                async with connection.cursor() as cursor:
+                    await cursor.execute(
+                        query="""
+                            CREATE TABLE IF NOT EXISTS users(
+                            id SERIAL PRIMARY KEY,
+                            user_id BIGINT NOT NULL UNIQUE,
+                            username VARCHAR(50),
+                            created_at
+                            )
+                            """
+                    )
